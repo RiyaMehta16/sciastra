cbb3a9fcc5fe53cd39a5e84ffcff91c9926d553c
Riya Mehta <116014331+RiyaMehta16@users.noreply.github.com>
2024-11-06 14:27:14 +0530

    Update settings.py
    security key

--- sciastra/settings.py
+++ sciastra/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6-bxv=+8q^4ovr**hx#e%x9uyehrh7z3v1h@&ceb-eh#ydaf@_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
